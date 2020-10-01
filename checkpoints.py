afa691bd40c6bf2954280def077424fcfda3e344
saaruuna <uuna.saarela@gmail.com>
2020-10-01 16:11:58 +0300

    Improve layout

--- checkpoints.py
+++ checkpoints.py
@@ -15,6 +15,11 @@ def get_names():
     list = result.fetchall()
     return list
 
+def get_checkpoint_id(name):
+    sql = "SELECT checkpoint_id from checkpoint WHERE name=:name"
+    result = db.session.execute(sql)
+    return result
+
 def perform_checkpoint(data, user_id, checkpoint_id):
     sql = "INSERT INTO performances (data, user_id, checkpoint_id) VALUES (:data,:user_id,:checkpoint_id)"
     db.session.execute(sql, {"data":data,"user_id":user_id,"checkpoint_id":checkpoint_id})
--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import redirect, render_template, request, session, flash
 from db import db
+from werkzeug.utils import secure_filename
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 import users
 import checkpoints
-
 
 @app.route("/")
 def index():
@@ -19,7 +20,7 @@ def login():
         if users.login(username,password):
             return redirect("/profile")
         else:
-            return render_template("error.html",message="Incorrect username or password")
+            return render_template("login.html",message="Incorrect username or password!")
 
 @app.route("/logout")
 def logout():
@@ -36,31 +37,29 @@ def register():
         if users.register(username,password):
             return redirect("/")
         else:
-            return render_template("error.html",message="Unable to create an account")
+            return render_template("register.html",message="Unable to create an account!")
+
+@app.route("/profile", methods=["GET","POST"])
+def profile():
+    if request.method == "GET":
+        return render_template("profile.html")
+    if request.method == "POST":
+        checkpoint = request.form["checkpoint"]
+        return render_template("profile.html", message="You successfully performed " + checkpoint "!")
 
 @app.route("/checkpoints")
 def showCheckpoints():
+    user_id = users.user_id()
     checkpointsList = checkpoints.get_checkpoints()
     return render_template("checkpoints.html", checkpointsList=checkpointsList)
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
 
 @app.route("/perform")
 def perform():
     checkpointNames = checkpoints.get_names()
     user_id = users.user_id()
-    checkpoint_id = 4;
-    file = request.files.get("file")
-    checkpoints.perform_checkpoint(file, user_id, checkpoint_id)
 
     return render_template("perform.html", checkpointNames=checkpointNames)
 
-@app.route("/result", methods=["POST"])
-def result():
-    checkpoint = request.form["checkpoint"]
-    return render_template("result.html", checkpoint=checkpoint)
 
 @app.route("/gallery")
 def gallery():
