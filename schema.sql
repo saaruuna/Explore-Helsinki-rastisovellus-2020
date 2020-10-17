09c9b90c165f159e0d7a15e9dc8fcf4a045922ab
saaruuna <uuna.saarela@gmail.com>
2020-10-17 11:08:06 +0300

    Improve schema layout

--- schema.sql
+++ schema.sql
@@ -1,14 +1,17 @@
 CREATE TABLE users (user_id SERIAL PRIMARY KEY,
   username TEXT UNIQUE,
-  password TEXT);
+  password TEXT
+);
 CREATE TABLE checkpoints (checkpoint_id SERIAL PRIMARY KEY,
   name TEXT,
   category TEXT,
-  description TEXT);
+  description TEXT
+);
 CREATE TABLE performances (performance_id SERIAL,
   data BYTEA,
   user_id SERIAL,
   checkpoint_id SERIAL,
   PRIMARY KEY(performance_id),
   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id),
-  CONSTRAINT fk_checkpoint FOREIGN KEY(checkpoint_id) REFERENCES checkpoints(checkpoint_id));
+  CONSTRAINT fk_checkpoint FOREIGN KEY(checkpoint_id) REFERENCES checkpoints(checkpoint_id)
+);
